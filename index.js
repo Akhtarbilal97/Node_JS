ea79952457f12d78515990c75da4d55b54410c5d
Akhtarbilal97 <se.bilal97@gmail.com>
2022-12-14 06:27:00 -0800

    iniital commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+const reqFilter=(req,resp,next)=>{
+   if(!req.query.age){
+      resp.send("Please Provide Pagee");
+   }
+
+   else if(req.query.age<20){
+      resp.send("You cant not access the page");
+   }
+   else{
+      next();
+   }
+
+}
+// restriction all page
+// app.use(reqFilter);
+
+
+app.get('',(req, resp)=>{
+  resp.send("Welcome to Home Page");
+});
+//  Apply middleware on one page just add reqFilter after user
+app.get('/user',reqFilter,(req, resp)=>{
+   resp.send("Welcome to User Page");
+ })
+
+
+app.listen(5000);
